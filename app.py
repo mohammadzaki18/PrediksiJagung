16911e2b0cc6d1a7ef86017f7cfef7b4e618d26e
mohammadzaki18 <mohammadzakijack@gmail.com>
2023-12-04 10:15:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Aplikasi Klasifikasi Tipe Jagung")
+st.write('''
+Aplikasi ini adalah model klasifikasi untuk memprediksi kualitas Jagung
+         
+         ''')
